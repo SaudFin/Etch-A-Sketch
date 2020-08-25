1eaf2306b9a7d88dc3b08405acccde71fccb0c52
SaudFin <developer.saud@gmail.com>
2020-08-25 06:14:19 +0300

    hover effect over the grid is active to leave a trail of orange background color

--- main.js
+++ main.js
@@ -5,10 +5,17 @@ const createGrid = () => {
     for (let n = 0; n < 16; n++) {
       let grid = document.createElement("div");
       grid.className = "grid";
-      console.log(container);
       container.appendChild(grid);
     }
   }
 };
-
 createGrid();
+
+let grids = document.getElementsByClassName("grid");
+for (let i = 0; i < grids.length; i++) {
+  grids[i].addEventListener("mouseover", function(event) {
+    event.target.style.backgroundColor = "orange";
+  });
+}
+
+// grids.addEventListener;
