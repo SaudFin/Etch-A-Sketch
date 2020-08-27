f3e1a8e76eb1dab5c5c0627aa62d3b7f9142c934
SaudFin <developer.saud@gmail.com>
2020-08-27 06:07:26 +0300

    reset functionality is working

--- main.js
+++ main.js
@@ -1,21 +1,35 @@
 let container = document.getElementById("container");
+let button = document.querySelector("button");
 
-const createGrid = () => {
-  for (let i = 0; i < 16; i++) {
-    for (let n = 0; n < 16; n++) {
+// to create the squares inside the grid
+const createGrid = num => {
+  container.innerHTML = "";
+  container.style.gridTemplateColumns = `repeat(${num},auto)`;
+  for (let i = 0; i < num; i++) {
+    for (let n = 0; n < num; n++) {
       let grid = document.createElement("div");
-      grid.className = "grid";
       container.appendChild(grid);
+      grid.className = "grid";
     }
   }
+
+  const grids = document.querySelectorAll(".grid");
+
+  for (let i = 0; i < grids.length; i++) {
+    grids[i].addEventListener("mouseover", function() {
+      grids[i].style.backgroundColor = "orange";
+    });
+  }
 };
-createGrid();
 
-let grids = document.getElementsByClassName("grid");
-for (let i = 0; i < grids.length; i++) {
-  grids[i].addEventListener("mouseover", function(event) {
-    event.target.style.backgroundColor = "orange";
-  });
-}
+// this function to make each grid changes color to orange when mouse is run over
 
-// grids.addEventListener;
+createGrid(16);
+
+const newGrid = () => {
+  let userGrid = prompt(
+    "Please enter number for how the size of the grid?",
+    "16, if you want a 16x16"
+  );
+  createGrid(userGrid);
+};
